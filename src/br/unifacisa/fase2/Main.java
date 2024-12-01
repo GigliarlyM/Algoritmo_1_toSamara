5bf0750d73fb15c0ef1e54d1c6996fb678fffea0
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2024-12-01 10:48:06 -0300

    Iniciando projeto

---
+++ src/br/unifacisa/fase2/Main.java
@@ -0,0 +1,10 @@
+package br.unifacisa.fase2;
+
+/* Resolver o problema da mochila de capacidade de 10KG
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inicio do codigo");
+    }
+}
